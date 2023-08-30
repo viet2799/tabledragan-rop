a1f9346a25826a4210942d4b47e98f590192e898
viet2799 <nguyenducviet@gmail.com>
2023-08-30 22:50:25 +0700

    .

---
+++ src/test/data.js
@@ -0,0 +1,110 @@
+export const periodData = [
+    {
+        id: 1,
+        name: "Tiết 1",
+    },
+    {
+        id: 2,
+        name: "Tiết 2",
+    },
+    {
+        id: 3,
+        name: "Tiết 3",
+    },
+    {
+        id: 4,
+        name: "Tiết 4",
+    },
+    {
+        id: 5,
+        name: "Tiết 5",
+    },
+    {
+        id: 6,
+        name: "Tiết 6",
+    },
+    {
+        id: 7,
+        name: "Tiết 7",
+    },
+    {
+        id: 8,
+        name: "Tiết 8",
+    },
+    {
+        id: 9,
+        name: "Tiết 9",
+    },
+];
+
+export const lessonData = [
+    {
+        id: 1,
+        name: "Đảng cách mạng Hồ Chí Minh",
+        number: 3,
+    },
+    {
+        id: 2,
+        name: "Triết",
+        number: 2,
+    },
+    {
+        id: 3,
+        name: "Triết 2",
+        number: 4,
+    },
+    {
+        id: 4,
+        name: "Giải tích 1",
+        number: 1,
+    },
+    {
+        id: 5,
+        name: "Giải tích 2",
+        number: 3,
+    },
+    {
+        id: 6,
+        name: "Đại số tuyến tính",
+        number: 2,
+    },
+];
+
+export const scheduleData = [
+    {
+        id: 1,
+        idTietBatDau: 2,
+        idTietKetThuc: 4,
+        lessonId: 1,
+    },
+    {
+        id: 2,
+        idTietBatDau: 3,
+        idTietKetThuc: 4,
+        lessonId: 2,
+    },
+    {
+        id: 3,
+        idTietBatDau: 3,
+        idTietKetThuc: 5,
+        lessonId: 5,
+    },
+    {
+        id: 4,
+        idTietBatDau: 2,
+        idTietKetThuc: 3,
+        lessonId: 6,
+    },
+    {
+        id: 5,
+        idTietBatDau: 6,
+        idTietKetThuc: 8,
+        lessonId: 1,
+    },
+    {
+        id: 6,
+        idTietBatDau: 7,
+        idTietKetThuc: 8,
+        lessonId: 6,
+    }
+]
